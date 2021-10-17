85367e44c86543b666d0e3b2a0103ec08d58a72a
蔡聿善 <jimmy012403976@gmail.com>
2021-10-17 19:15:54 +0800

    add rsyslog script

--- Linux/bash/iptablesEmailNotification.sh
+++ Linux/bash/iptablesEmailNotification.sh
@@ -8,63 +8,36 @@ splitinspace()
 	done
 }
 
-data=`tail -n 1 /var/log/iptables`
-bfsrc=""
-bfpro=""
-bfport=""
-count=0
-while : 
-do
-	allline="`cat /var/log/iptables`"
-	cont=1
-
-	maxcont="`echo "$allline" | wc -l`"
-
-	while [ "`echo "$allline" | tac | sed -n ${cont}p`" != "$data" ] && [ "$cont" -le $maxcont ]
-	do
-		(( cont++ ))
-	done
-	(( cont-- ))
-	
-#	echo $cont
-
-	for a in $(seq $cont -1 1)
-	do
-		nowdata=`echo "$allline" | tac | sed -n ${a}p`
-		src=`echo $nowdata | splitinspace | grep SRC | cut -c 5-`
-		pro=`echo $nowdata | splitinspace | grep PROTO | cut -c 7-`
-		port=`echo $nowdata | splitinspace | grep DPT | cut -c 5-`
-		if [ "$bfsrc" != "$src" ] || [ "$bfpro" != "$pro" ] || [ "$bfport" != "$port" ]
-		then
-			service=`grep -i "	$port/$pro" /etc/myserviceslist | awk '{print $1}'`
-			#echo $service
-			if [ "$service" = "" ]
-			then
-				service="$port/$pro port"
-			else
-				service="$service($port/$pro) service"
-			fi
-			echo "The $service has been ACCEPT from ip=$src"
-			echo "The $service has been ACCEPT from ip=$src" | su jimmygw -c "mail -s \"iptables notification\" jimmy012403976@gmail.com"
-			count=1
-			#echo send
-		fi
-		data=$nowdata
-		bfsrc=$src
-		bfpro=$pro
-		bfport=$port
-		#echo $data
-	done
-	if [ "$count" -ge 300 ]
+lastlog=`cat /etc/iptables/lastiptableslog`
+src=`echo $1 | splitinspace | grep SRC | cut -c 5-`
+pro=`echo $1 | splitinspace | grep PROTO | cut -c 7-`
+port=`echo $1 | splitinspace | grep DPT | cut -c 5-`
+nowtime=`date +"%s"`
+if [ "$lastlog" == "" ]
+then
+	bfsrc=0
+	bfpro=0
+	bfport=0
+	bftime=0
+else
+	bfsrc=`echo $lastlog | splitinspace | grep SRC | cut -c 5-`
+	bfpro=`echo $lastlog | splitinspace | grep PROTO | cut -c 7-`
+	bfport=`echo $lastlog | splitinspace | grep DPT | cut -c 5-`
+	bftime=`echo $lastlog | splitinspace | grep TIME | cut -c 6-`
+fi
+if [ "$bfsrc" != "$src" ] || [ "$bfpro" != "$pro" ] || [ "$bfport" != "$port" ] || [ $(( $nowtime - $bftime )) -ge 60 ]
+then
+	service=`grep -i "	$port/$pro" /etc/myserviceslist | awk '{print $1}'`
+	#echo $service
+	if [ "$service" = "" ]
 	then
-		bfsrc=""
-		bfpro=""
-		bfport=""
-		#echo $count
-		count=0
-	elif [ "$count" -ge 1 ]
-	then
-		count=$(($count + 1))
+		service="$port/$pro port"
+	else
+		service="$service($port/$pro) service"
 	fi
-	sleep 0.1
-done
+	echo "The $service has been ACCEPT from ip=$src"
+	echo "The $service has been ACCEPT from ip=$src" | su jimmygw -c "mail -s \"iptables notification\" jimmy012403976@gmail.com"
+
+	echo "TIME=$nowtime $1" > /etc/iptables/lastiptableslog
+	#echo send
+fi
